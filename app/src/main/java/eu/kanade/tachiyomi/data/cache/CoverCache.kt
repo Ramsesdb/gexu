cuong-tran/tachiyomiJ2K

use getFavoriteMangas instead of getLibraryMangas where possible

app/src/main/java/eu/kanade/tachiyomi/data/cache/CoverCache.kt
@@ -69,7 +69,7 @@ class CoverCache(val context: Context) {
     suspend fun deleteOldCovers() {
         val db = Injekt.get<DatabaseHelper>()
         var deletedSize = 0L
-        val urls = db.getLibraryMangas().executeOnIO().mapNotNull {
+        val urls = db.getFavoriteMangas().executeOnIO().mapNotNull {
             it.thumbnail_url?.let { url -> return@mapNotNull DiskUtil.hashKeyForDisk(url) }
             null
         }

Ramsesdb/gexu

(cherry picked from commit f913c3ab5e0e466a9f266bb94378bd342a34f776)

app/src/main/java/eu/kanade/tachiyomi/data/cache/CoverCache.kt
@@ -72,7 +72,7 @@ class CoverCache(val context: Context) {
     suspend fun deleteOldCovers() {
         val db = Injekt.get<DatabaseHelper>()
         var deletedSize = 0L
-            val urls = db.getLibraryMangaList().executeOnIO().mapNotNull {
+        val urls = db.getFavoriteMangaList().executeOnIO().mapNotNull {
             it.thumbnail_url?.let { url -> return@mapNotNull DiskUtil.hashKeyForDisk(url) }
             null
         }