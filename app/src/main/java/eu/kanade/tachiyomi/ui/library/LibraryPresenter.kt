cuong-tran/tachiyomiJ2K

Use getFavoriteMangas for updateRatiosAndColors

app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryPresenter.kt
@@ -1223,7 +1223,7 @@ class LibraryPresenter(
         suspend fun updateRatiosAndColors() {
             val db: DatabaseHelper = Injekt.get()
             val mangaFetcher = MangaFetcher()
-            val libraryManga = db.getLibraryMangas().executeOnIO()
+            val libraryManga = db.getFavoriteMangas().executeOnIO()
             libraryManga.forEach { manga ->
                 mangaFetcher.setRatioAndColors(manga)
             }

Ramsesdb/gexu

(cherry picked from commit a5541e43e0068e43591131d93a687b6e1e6ceec1)

app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryPresenter.kt
@@ -1264,7 +1264,7 @@ class LibraryPresenter(
         suspend fun updateRatiosAndColors() {
             val db: DatabaseHelper = Injekt.get()
             val mangaFetcher = MangaFetcher()
-            val libraryManga = db.getLibraryMangaList().executeOnIO()
+            val libraryManga = db.getFavoriteMangaList().executeOnIO()
             libraryManga.forEach { manga ->
                 mangaFetcher.setRatioAndColors(manga)
             }