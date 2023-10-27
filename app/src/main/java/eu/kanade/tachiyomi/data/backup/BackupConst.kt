Ramsesdb/gexu-legacy-fork

Add source preferences to backups
Closes #1857

Co-authored-by: jmir1 <jmir1@users.noreply.github.com>

app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupConst.kt
@@ -17,5 +17,8 @@ internal object BackupConst {
     const val BACKUP_APP_PREFS = 0x10
     const val BACKUP_APP_PREFS_MASK = 0x10
 
-    const val BACKUP_ALL = 0x1F
+    const val BACKUP_SOURCE_PREFS = 0x20
+    const val BACKUP_SOURCE_PREFS_MASK = 0x20
+
+    const val BACKUP_ALL = 0x3F
 }

Ramsesdb/gexu

(cherry picked from commit 0f42b9f1544b188362d1c0046c8ff853827ed270)
# Conflicts:
#	app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsBackupScreen.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupConst.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupManager.kt
#	app/src/main/java/eu/kanade/tachiyomi/data/backup/models/Backup.kt

app/src/main/java/eu/kanade/tachiyomi/data/backup/BackupConst.kt
@@ -17,13 +17,16 @@ internal object BackupConst {
     const val BACKUP_APP_PREFS = 0x10
     const val BACKUP_APP_PREFS_MASK = 0x10
 
+    const val BACKUP_SOURCE_PREFS = 0x20
+    const val BACKUP_SOURCE_PREFS_MASK = 0x20
+
     // SY -->
-    const val BACKUP_CUSTOM_INFO = 0x20
-    const val BACKUP_CUSTOM_INFO_MASK = 0x20
+    const val BACKUP_CUSTOM_INFO = 0x40
+    const val BACKUP_CUSTOM_INFO_MASK = 0x40
 
-    const val BACKUP_READ_MANGA = 0x40
-    const val BACKUP_READ_MANGA_MASK = 0x40
+    const val BACKUP_READ_MANGA = 0x80
+    const val BACKUP_READ_MANGA_MASK = 0x80
 
-    const val BACKUP_ALL = 0x7F
+    const val BACKUP_ALL = 0xFF
     // SY <--
 }