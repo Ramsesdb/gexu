Ramsesdb/gexu-legacy-fork

Group mode dialogs together in bottom reader bar

app/src/main/java/eu/kanade/presentation/reader/appbars/BottomReaderBar.kt
@@ -46,17 +46,17 @@ fun BottomReaderBar(
             )
         }
 
+        IconButton(onClick = onClickOrientation) {
+            Icon(
+                painter = painterResource(orientation.iconRes),
+                contentDescription = stringResource(R.string.rotation_type),
+            )
+        }
+
         IconButton(onClick = onClickCropBorder) {
             Icon(
                 painter = painterResource(if (cropEnabled) R.drawable.ic_crop_24dp else R.drawable.ic_crop_off_24dp),
                 contentDescription = stringResource(R.string.pref_crop_borders),
-            )
-        }
-
-        IconButton(onClick = onClickOrientation) {
-            Icon(
-                painter = painterResource(orientation.iconRes),
-                contentDescription = stringResource(R.string.pref_rotation_type),
             )
         }
 

Ramsesdb/gexu

(cherry picked from commit 3a8aa3e8cd7b725ee73b7fe67d0b1c3111af977b)
# Conflicts:
#	app/src/main/java/eu/kanade/presentation/reader/appbars/BottomReaderBar.kt

app/src/main/java/eu/kanade/presentation/reader/appbars/BottomReaderBar.kt
@@ -92,6 +92,15 @@ fun BottomReaderBar(
             }
         }
 
+        if (ReaderBottomButton.Rotation.isIn(enabledButtons)) {
+            IconButton(onClick = onClickOrientation) {
+                Icon(
+                    painter = painterResource(orientation.iconRes),
+                    contentDescription = stringResource(R.string.pref_rotation_type),
+                )
+            }
+        }
+
         val cropBorders = when (readingMode) {
             ReadingMode.WEBTOON -> ReaderBottomButton.CropBordersWebtoon
             ReadingMode.CONTINUOUS_VERTICAL -> ReaderBottomButton.CropBordersContinuesVertical
@@ -102,15 +111,6 @@ fun BottomReaderBar(
                 Icon(
                     painter = painterResource(if (cropEnabled) R.drawable.ic_crop_24dp else R.drawable.ic_crop_off_24dp),
                     contentDescription = stringResource(R.string.pref_crop_borders),
-                )
-            }
-        }
-
-        if (ReaderBottomButton.Rotation.isIn(enabledButtons)) {
-            IconButton(onClick = onClickOrientation) {
-                Icon(
-                    painter = painterResource(orientation.iconRes),
-                    contentDescription = stringResource(R.string.pref_rotation_type),
                 )
             }
         }