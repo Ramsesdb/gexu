Ramsesdb/gexu-legacy-fork

Make tracker use Mihon's client

app/src/main/java/eu/kanade/tachiyomi/data/track/myanimelist/MyAnimeListApi.kt
@@ -278,7 +278,7 @@ class MyAnimeListApi(
 
     companion object {
         // Registered under arkon's MAL account
-        private const val clientId = "8fd3313bc138e8b890551aa1de1a2589"
+        private const val clientId = "f46004a9c16483b6d87b5bf10de56d97"
 
         private const val baseOAuthUrl = "https://myanimelist.net/v1/oauth2"
         private const val baseApiUrl = "https://api.myanimelist.net/v2"

Ramsesdb/gexu

(cherry picked from commit 8c910f2a2c27dc339a4b05f03be3386a3afa2194)

app/src/main/java/eu/kanade/tachiyomi/data/track/myanimelist/MyAnimeListApi.kt
@@ -277,7 +277,7 @@ class MyAnimeListApi(
     }
 
     companion object {
-        private const val CLIENT_ID = "8fd3313bc138e8b890551aa1de1a2589"
+        private const val CLIENT_ID = "c46c9e24640a64dad5be5ca7a1a53a0f"
 
         private const val BASE_OAUTH_URL = "https://myanimelist.net/v1/oauth2"
         private const val BASE_API_URL = "https://api.myanimelist.net/v2"