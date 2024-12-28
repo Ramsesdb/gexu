Ramsesdb/gexu

commit a0fc8f27d405a5d46a2c69b28fbf7482b66aaf8f

refactor: Use Compose for About page

app/src/main/java/eu/kanade/tachiyomi/ui/more/AboutController.kt
@@ -1,173 +1,53 @@
 package eu.kanade.tachiyomi.ui.more
 
 import android.app.Dialog
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.TextView
-import androidx.core.content.getSystemService
-import androidx.core.net.toUri
-import androidx.preference.PreferenceScreen
-import co.touchlab.kermit.Logger
-import eu.kanade.tachiyomi.BuildConfig
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import cafe.adriel.voyager.core.stack.StackEvent
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.ScreenTransition
 import eu.kanade.tachiyomi.data.updater.AppDownloadInstallJob
-import eu.kanade.tachiyomi.data.updater.AppUpdateChecker
-import eu.kanade.tachiyomi.data.updater.AppUpdateNotifier
-import eu.kanade.tachiyomi.data.updater.AppUpdateResult
-import eu.kanade.tachiyomi.data.updater.RELEASE_URL
+import eu.kanade.tachiyomi.ui.base.controller.BaseComposeController
 import eu.kanade.tachiyomi.ui.base.controller.DialogController
-import eu.kanade.tachiyomi.ui.setting.SettingsLegacyController
-import eu.kanade.tachiyomi.ui.setting.add
-import eu.kanade.tachiyomi.ui.setting.onClick
-import eu.kanade.tachiyomi.ui.setting.preference
-import eu.kanade.tachiyomi.ui.setting.preferenceCategory
-import eu.kanade.tachiyomi.ui.setting.titleMRes
-import eu.kanade.tachiyomi.util.CrashLogUtil
-import eu.kanade.tachiyomi.util.lang.toTimestampString
-import eu.kanade.tachiyomi.util.system.isOnline
-import eu.kanade.tachiyomi.util.system.localeContext
+import eu.kanade.tachiyomi.util.compose.LocalAlertDialog
+import eu.kanade.tachiyomi.util.compose.LocalBackPress
 import eu.kanade.tachiyomi.util.system.materialAlertDialog
-import eu.kanade.tachiyomi.util.system.toast
 import eu.kanade.tachiyomi.util.view.setNegativeButton
 import eu.kanade.tachiyomi.util.view.setPositiveButton
 import eu.kanade.tachiyomi.util.view.setTitle
-import eu.kanade.tachiyomi.util.view.snack
-import eu.kanade.tachiyomi.util.view.withFadeTransaction
 import io.noties.markwon.Markwon
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import soup.compose.material.motion.animation.materialSharedAxisZ
+import yokai.domain.ComposableAlertDialog
 import yokai.i18n.MR
-import yokai.util.lang.getString
-import java.text.DateFormat
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.*
+import yokai.presentation.settings.screen.about.AboutScreen
 import android.R as AR
 
-class AboutController : SettingsLegacyController() {
+class AboutController : BaseComposeController() {
 
-    /**
-     * Checks for new releases
-     */
-    private val updateChecker by lazy { AppUpdateChecker() }
-
-    private val dateFormat: DateFormat by lazy {
-        preferences.dateFormat()
-    }
-
-    private val isUpdaterEnabled = BuildConfig.INCLUDE_UPDATER
-
-    override fun setupPreferenceScreen(screen: PreferenceScreen) = screen.apply {
-        titleMRes = MR.strings.about
-
-        preference {
-            key = "pref_whats_new"
-            titleMRes = MR.strings.whats_new_this_release
-            onClick {
-                val intent = Intent(
-                    Intent.ACTION_VIEW,
-                    if (BuildConfig.DEBUG) {
-                        "https://github.com/null2264/yokai/commits/master"
-                    } else {
-                        RELEASE_URL
-                    }.toUri(),
-                )
-                startActivity(intent)
-            }
-        }
-        if (isUpdaterEnabled) {
-            preference {
-                key = "pref_check_for_updates"
-                titleMRes = MR.strings.check_for_updates
-                onClick {
-                    if (activity!!.isOnline()) {
-                        checkVersion()
-                    } else {
-                        activity!!.toast(MR.strings.no_network_connection)
-                    }
+    @Composable
+    override fun ScreenContent() {
+        Navigator(
+            screen = AboutScreen { body, url, isBeta ->
+                NewUpdateDialogController(body, url, isBeta).showDialog(router)
+            },
+            content = {
+                CompositionLocalProvider(
+                    LocalAlertDialog provides ComposableAlertDialog(null),
+                    LocalBackPress provides router::handleBack,
+                ) {
+                    ScreenTransition(
+                        navigator = it,
+                        // FIXME: Mimic J2K's Conductor transition
+                        transition = { materialSharedAxisZ(forward = it.lastEvent != StackEvent.Pop) },
+                    )
                 }
-            }
-        }
-        preference {
-            key = "pref_version"
-            titleMRes = MR.strings.version
-            summary = if (BuildConfig.DEBUG || BuildConfig.NIGHTLY) {
-                "r" + BuildConfig.COMMIT_COUNT
-            } else {
-                BuildConfig.VERSION_NAME
-            }
-
-            onClick {
-                activity?.let {
-                    val deviceInfo = CrashLogUtil(it.localeContext).getDebugInfo()
-                    val clipboard = it.getSystemService<ClipboardManager>()!!
-                    val appInfo = it.getString(MR.strings.app_info)
-                    clipboard.setPrimaryClip(ClipData.newPlainText(appInfo, deviceInfo))
-                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-                        view?.snack(context.getString(MR.strings._copied_to_clipboard, appInfo))
-                    }
-                }
-            }
-        }
-        preference {
-            key = "pref_build_time"
-            titleMRes = MR.strings.build_time
-            summary = getFormattedBuildTime(dateFormat)
-        }
-
-        preferenceCategory {
-            preference {
-                key = "pref_oss"
-                titleMRes = MR.strings.open_source_licenses
-
-                onClick {
-                    router.pushController(AboutLicenseController().withFadeTransaction())
-                }
-            }
-        }
-        add(AboutLinksPreference(context))
-    }
-
-    /**
-     * Checks version and shows a user prompt if an update is available.
-     */
-    private fun checkVersion() {
-        val activity = activity ?: return
-
-        activity.toast(MR.strings.searching_for_updates)
-        viewScope.launch {
-            val result = try {
-                updateChecker.checkForUpdate(activity, true)
-            } catch (error: Exception) {
-                withContext(Dispatchers.Main) {
-                    activity.toast(error.message)
-                    Logger.e(error) { "Couldn't check new update" }
-                }
-            }
-            when (result) {
-                is AppUpdateResult.NewUpdate -> {
-                    val body = result.release.info
-                    val url = result.release.downloadLink
-                    val isBeta = result.release.preRelease == true
-
-                    // Create confirmation window
-                    withContext(Dispatchers.Main) {
-                        AppUpdateNotifier.releasePageUrl = result.release.releaseLink
-                        NewUpdateDialogController(body, url, isBeta).showDialog(router)
-                    }
-                }
-                is AppUpdateResult.NoNewUpdate -> {
-                    withContext(Dispatchers.Main) {
-                        activity.toast(MR.strings.no_new_updates_available)
-                    }
-                }
-            }
-        }
+            },
+        )
     }
 
     class NewUpdateDialogController(bundle: Bundle? = null) : DialogController(bundle) {
@@ -218,21 +98,6 @@ class AboutController : SettingsLegacyController() {
             const val BODY_KEY = "NewUpdateDialogController.body"
             const val URL_KEY = "NewUpdateDialogController.key"
             const val IS_BETA = "NewUpdateDialogController.is_beta"
-        }
-    }
-
-    companion object {
-        fun getFormattedBuildTime(dateFormat: DateFormat): String {
-            try {
-                val inputDf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.getDefault())
-                inputDf.timeZone = TimeZone.getTimeZone("UTC")
-                val buildTime =
-                    inputDf.parse(BuildConfig.BUILD_TIME) ?: return BuildConfig.BUILD_TIME
-
-                return buildTime.toTimestampString(dateFormat)
-            } catch (e: ParseException) {
-                return BuildConfig.BUILD_TIME
-            }
         }
     }
 }

app/src/main/java/yokai/presentation/settings/screen/about/AboutScreen.kt
@@ -1,0 +1,239 @@
+package yokai.presentation.settings.screen.about
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Build
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Public
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.unit.dp
+import androidx.core.content.getSystemService
+import cafe.adriel.voyager.navigator.LocalNavigator
+import co.touchlab.kermit.Logger
+import dev.icerock.moko.resources.compose.stringResource
+import eu.kanade.tachiyomi.BuildConfig
+import eu.kanade.tachiyomi.core.storage.preference.asDateFormat
+import eu.kanade.tachiyomi.data.preference.PreferencesHelper
+import eu.kanade.tachiyomi.data.updater.AppUpdateChecker
+import eu.kanade.tachiyomi.data.updater.AppUpdateNotifier
+import eu.kanade.tachiyomi.data.updater.AppUpdateResult
+import eu.kanade.tachiyomi.data.updater.RELEASE_URL
+import eu.kanade.tachiyomi.util.CrashLogUtil
+import eu.kanade.tachiyomi.util.compose.currentOrThrow
+import eu.kanade.tachiyomi.util.lang.toTimestampString
+import eu.kanade.tachiyomi.util.system.isOnline
+import eu.kanade.tachiyomi.util.system.localeContext
+import eu.kanade.tachiyomi.util.system.toast
+import eu.kanade.tachiyomi.util.system.withUIContext
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.TimeZone
+import kotlinx.coroutines.launch
+import uy.kohesive.injekt.injectLazy
+import yokai.i18n.MR
+import yokai.presentation.component.preference.widget.TextPreferenceWidget
+import yokai.presentation.core.components.LinkIcon
+import yokai.presentation.core.enterAlwaysCollapsedScrollBehavior
+import yokai.presentation.core.icons.CustomIcons
+import yokai.presentation.core.icons.Discord
+import yokai.presentation.core.icons.GitHub
+import yokai.presentation.settings.SettingsScaffold
+import yokai.util.Screen
+import yokai.util.lang.getString
+
+class AboutScreen(private val showNewUpdateDialog: (String, String, Boolean?) -> Unit) : Screen() {
+    @Composable
+    override fun Content() {
+        val context = LocalContext.current
+        val navigator = LocalNavigator.currentOrThrow
+        val uriHandler = LocalUriHandler.current
+
+        val snackbarHostState = remember { SnackbarHostState() }
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+
+        val preferences: PreferencesHelper by injectLazy()
+        val dateFormat by lazy { preferences.dateFormatRaw().get().asDateFormat() }
+
+        SettingsScaffold(
+            title = stringResource(MR.strings.about),
+            snackbarHost = {
+                SnackbarHost(hostState = snackbarHostState)
+            },
+            appBarScrollBehavior = enterAlwaysCollapsedScrollBehavior(
+                state = rememberTopAppBarState(),
+                canScroll = { listState.canScrollForward || listState.canScrollBackward },
+                isAtTop = { listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0 },
+            ),
+            content = { contentPadding ->
+                LazyColumn(
+                    contentPadding = contentPadding,
+                    state = listState,
+                ) {
+                    item {
+                        TextPreferenceWidget(
+                            title = stringResource(MR.strings.whats_new_this_release),
+                            onPreferenceClick = {
+                                uriHandler.openUri(if (BuildConfig.DEBUG) SOURCE_URL else RELEASE_URL)
+                            },
+                        )
+                    }
+
+                    if (BuildConfig.INCLUDE_UPDATER) {
+                        item {
+                            TextPreferenceWidget(
+                                title = stringResource(MR.strings.check_for_updates),
+                                onPreferenceClick = {
+                                    if (context.isOnline()) {
+                                        scope.launch {
+                                            context.checkVersion()
+                                        }
+                                    } else {
+                                        context.toast(MR.strings.no_network_connection)
+                                    }
+                                },
+                            )
+                        }
+                    }
+
+                    item {
+                        TextPreferenceWidget(
+                            title = stringResource(MR.strings.version),
+                            subtitle = getVersionName(),
+                            onPreferenceClick = {
+                                val deviceInfo = CrashLogUtil(context.localeContext).getDebugInfo()
+                                val clipboard = context.getSystemService<ClipboardManager>()!!
+                                val appInfo = context.getString(MR.strings.app_info)
+                                clipboard.setPrimaryClip(ClipData.newPlainText(appInfo, deviceInfo))
+                                scope.launch {
+                                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+                                        snackbarHostState.showSnackbar(
+                                            message = context.getString(MR.strings._copied_to_clipboard, appInfo),
+                                        )
+                                    }
+                                }
+                            },
+                        )
+                    }
+
+                    item {
+                        TextPreferenceWidget(
+                            title = stringResource(MR.strings.version),
+                            subtitle = getFormattedBuildTime(dateFormat),
+                        )
+                    }
+
+                    item {
+                        Column(modifier = Modifier.fillMaxWidth()) {
+                            HorizontalDivider()
+
+                            TextPreferenceWidget(
+                                title = stringResource(MR.strings.open_source_licenses),
+                                onPreferenceClick = { navigator.push(AboutLicenseScreen()) }
+                            )
+                        }
+                    }
+
+                    item {
+                        FlowRow(
+                            modifier =
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 8.dp),
+                            horizontalArrangement = Arrangement.Center,
+                        ) {
+                            LinkIcon(
+                                label = "Website",
+                                icon = Icons.Outlined.Public,
+                                url = "https://mihon.app",
+                            )
+                            LinkIcon(
+                                label = "Discord",
+                                icon = CustomIcons.Discord,
+                                url = "https://discord.gg/mihon",
+                            )
+                            LinkIcon(
+                                label = "GitHub",
+                                icon = CustomIcons.GitHub,
+                                url = "https://github.com/null2264/yokai",
+                            )
+                        }
+                    }
+                }
+            },
+        )
+    }
+
+    private fun getVersionName(): String = when {
+        BuildConfig.DEBUG -> "Debug ${BuildConfig.COMMIT_SHA}"
+        BuildConfig.NIGHTLY -> "Nightly ${BuildConfig.COMMIT_COUNT} (${BuildConfig.COMMIT_SHA})"
+        else -> "Release ${BuildConfig.VERSION_NAME}"
+    }
+
+    private suspend fun Context.checkVersion() {
+        val updateChecker = AppUpdateChecker()
+
+        withUIContext { toast(MR.strings.searching_for_updates) }
+
+        val result = try {
+            updateChecker.checkForUpdate(this, true)
+        } catch (error: Exception) {
+            withUIContext {
+                toast(error.message)
+                Logger.e(error) { "Couldn't check new update" }
+            }
+        }
+        when (result) {
+            is AppUpdateResult.NewUpdate -> {
+                val body = result.release.info
+                val url = result.release.downloadLink
+                val isBeta = result.release.preRelease == true
+
+                // Create confirmation window
+                withUIContext {
+                    AppUpdateNotifier.releasePageUrl = result.release.releaseLink
+                    showNewUpdateDialog(body, url, isBeta)
+                }
+            }
+            is AppUpdateResult.NoNewUpdate -> {
+                withUIContext {
+                    toast(MR.strings.no_new_updates_available)
+                }
+            }
+        }
+    }
+}
+
+fun getFormattedBuildTime(dateFormat: DateFormat): String {
+    try {
+        val inputDf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.getDefault())
+        inputDf.timeZone = TimeZone.getTimeZone("UTC")
+        val buildTime =
+            inputDf.parse(BuildConfig.BUILD_TIME) ?: return BuildConfig.BUILD_TIME
+
+        return buildTime.toTimestampString(dateFormat)
+    } catch (e: ParseException) {
+        return BuildConfig.BUILD_TIME
+    }
+}
+
+private const val SOURCE_URL = "https://github.com/null2264/yokai/commits/master"

app/src/main/java/eu/kanade/tachiyomi/core/storage/preference/PreferenceExtension.kt
@@ -5,9 +5,17 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import eu.kanade.tachiyomi.core.preference.Preference
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Composable
 fun <T> Preference<T>.collectAsState(): State<T> {
     val flow = remember(this) { changes() }
     return flow.collectAsState(initial = get())
+}
+
+fun String.asDateFormat(): DateFormat = when (this) {
+    "" -> DateFormat.getDateInstance(DateFormat.SHORT)
+    else -> SimpleDateFormat(this, Locale.getDefault())
 }

app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -7,6 +7,7 @@ import eu.kanade.tachiyomi.BuildConfig
 import eu.kanade.tachiyomi.core.preference.Preference
 import eu.kanade.tachiyomi.core.preference.PreferenceStore
 import eu.kanade.tachiyomi.core.preference.getEnum
+import eu.kanade.tachiyomi.core.storage.preference.asDateFormat
 import eu.kanade.tachiyomi.data.updater.AppDownloadInstallJob
 import eu.kanade.tachiyomi.domain.manga.models.Manga
 import eu.kanade.tachiyomi.extension.model.InstalledExtensionsOrder
@@ -19,13 +20,12 @@ import eu.kanade.tachiyomi.ui.reader.settings.ReadingModeType
 import eu.kanade.tachiyomi.ui.reader.viewer.ViewerNavigation
 import eu.kanade.tachiyomi.ui.recents.RecentsPresenter
 import eu.kanade.tachiyomi.util.system.Themes
+import java.text.DateFormat
+import java.util.Locale
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.*
 import eu.kanade.tachiyomi.data.preference.PreferenceKeys as Keys
 import eu.kanade.tachiyomi.data.preference.PreferenceValues as Values
 
@@ -187,10 +187,10 @@ class PreferencesHelper(val context: Context, val preferenceStore: PreferenceSto
 
     fun anilistScoreType() = preferenceStore.getString("anilist_score_type", "POINT_10")
 
-    fun dateFormat(format: String = preferenceStore.getString(Keys.dateFormat, "").get()): DateFormat = when (format) {
-        "" -> DateFormat.getDateInstance(DateFormat.SHORT)
-        else -> SimpleDateFormat(format, Locale.getDefault())
-    }
+    fun dateFormatRaw() = preferenceStore.getString(Keys.dateFormat, "")
+
+    @Deprecated("Use dateFormatRaw().get().asDateFormat() instead")
+    fun dateFormat(format: String = dateFormatRaw().get()): DateFormat = format.asDateFormat()
 
     fun appLanguage() = preferenceStore.getString("app_language", "")
 

app/src/main/java/eu/kanade/tachiyomi/ui/more/AboutLicenseController.kt
@@ -1,27 +1,0 @@
-package eu.kanade.tachiyomi.ui.more
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import cafe.adriel.voyager.core.stack.StackEvent
-import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.transitions.ScreenTransition
-import eu.kanade.tachiyomi.ui.base.controller.BaseComposeController
-import eu.kanade.tachiyomi.util.compose.LocalBackPress
-import soup.compose.material.motion.animation.materialSharedAxisZ
-
-class AboutLicenseController : BaseComposeController() {
-    @Composable
-    override fun ScreenContent() {
-        Navigator(
-            screen = AboutLicenseScreen(),
-            content = {
-                CompositionLocalProvider(LocalBackPress provides router::handleBack) {
-                    ScreenTransition(
-                        navigator = it,
-                        transition = { materialSharedAxisZ(forward = it.lastEvent != StackEvent.Pop) },
-                    )
-                }
-            },
-        )
-    }
-}

commit 318f145b14d13539428d0c52cfb8dca0ae93ebfe

fix: Crashes caused by a0fc8f27d405a5d46a2c69b28fbf7482b66aaf8f
Screen arguments need to be parcelable

app/src/main/java/eu/kanade/tachiyomi/ui/more/AboutController.kt
@@ -16,6 +16,7 @@ import eu.kanade.tachiyomi.ui.base.controller.BaseComposeController
 import eu.kanade.tachiyomi.ui.base.controller.DialogController
 import eu.kanade.tachiyomi.util.compose.LocalAlertDialog
 import eu.kanade.tachiyomi.util.compose.LocalBackPress
+import eu.kanade.tachiyomi.util.compose.LocalRouter
 import eu.kanade.tachiyomi.util.system.materialAlertDialog
 import eu.kanade.tachiyomi.util.view.setNegativeButton
 import eu.kanade.tachiyomi.util.view.setPositiveButton
@@ -32,13 +33,12 @@ class AboutController : BaseComposeController() {
     @Composable
     override fun ScreenContent() {
         Navigator(
-            screen = AboutScreen { body, url, isBeta ->
-                NewUpdateDialogController(body, url, isBeta).showDialog(router)
-            },
+            screen = AboutScreen(),
             content = {
                 CompositionLocalProvider(
                     LocalAlertDialog provides ComposableAlertDialog(null),
                     LocalBackPress provides router::handleBack,
+                    LocalRouter provides router,
                 ) {
                     ScreenTransition(
                         navigator = it,

app/src/main/java/eu/kanade/tachiyomi/util/compose/Locals.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.staticCompositionLocalOf
+import com.bluelinelabs.conductor.Router
 import yokai.domain.ComposableAlertDialog
 
 val <T> ProvidableCompositionLocal<T?>.currentOrThrow
@@ -12,3 +13,8 @@ val <T> ProvidableCompositionLocal<T?>.currentOrThrow
 
 val LocalBackPress: ProvidableCompositionLocal<(() -> Unit)?> = staticCompositionLocalOf { null }
 val LocalAlertDialog: ProvidableCompositionLocal<ComposableAlertDialog?> = compositionLocalOf { null }
+@Deprecated(
+    message = "Scheduled for removal once Conductor is fully replaced by Voyager",
+    replaceWith = ReplaceWith("LocalNavigator", "cafe.adriel.voyager.navigator.LocalNavigator"),
+)
+val LocalRouter: ProvidableCompositionLocal<Router?> = compositionLocalOf { null }

app/src/main/java/yokai/presentation/settings/screen/about/AboutScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.core.content.getSystemService
 import cafe.adriel.voyager.navigator.LocalNavigator
 import co.touchlab.kermit.Logger
+import com.bluelinelabs.conductor.Router
 import dev.icerock.moko.resources.compose.stringResource
 import eu.kanade.tachiyomi.BuildConfig
 import eu.kanade.tachiyomi.core.storage.preference.asDateFormat
@@ -35,7 +36,9 @@ import eu.kanade.tachiyomi.data.updater.AppUpdateChecker
 import eu.kanade.tachiyomi.data.updater.AppUpdateNotifier
 import eu.kanade.tachiyomi.data.updater.AppUpdateResult
 import eu.kanade.tachiyomi.data.updater.RELEASE_URL
+import eu.kanade.tachiyomi.ui.more.AboutController.NewUpdateDialogController
 import eu.kanade.tachiyomi.util.CrashLogUtil
+import eu.kanade.tachiyomi.util.compose.LocalRouter
 import eu.kanade.tachiyomi.util.compose.currentOrThrow
 import eu.kanade.tachiyomi.util.lang.toTimestampString
 import eu.kanade.tachiyomi.util.system.isOnline
@@ -60,11 +63,12 @@ import yokai.presentation.settings.SettingsScaffold
 import yokai.util.Screen
 import yokai.util.lang.getString
 
-class AboutScreen(private val showNewUpdateDialog: (String, String, Boolean?) -> Unit) : Screen() {
+class AboutScreen : Screen() {
     @Composable
     override fun Content() {
         val context = LocalContext.current
         val navigator = LocalNavigator.currentOrThrow
+        val router = LocalRouter.currentOrThrow
         val uriHandler = LocalUriHandler.current
 
         val snackbarHostState = remember { SnackbarHostState() }
@@ -105,7 +109,7 @@ class AboutScreen(private val showNewUpdateDialog: (String, String, Boolean?) ->
                                 onPreferenceClick = {
                                     if (context.isOnline()) {
                                         scope.launch {
-                                            context.checkVersion()
+                                            context.checkVersion(router)
                                         }
                                     } else {
                                         context.toast(MR.strings.no_network_connection)
@@ -189,7 +193,7 @@ class AboutScreen(private val showNewUpdateDialog: (String, String, Boolean?) ->
         else -> "Release ${BuildConfig.VERSION_NAME}"
     }
 
-    private suspend fun Context.checkVersion() {
+    private suspend fun Context.checkVersion(router: Router) {
         val updateChecker = AppUpdateChecker()
 
         withUIContext { toast(MR.strings.searching_for_updates) }
@@ -211,7 +215,7 @@ class AboutScreen(private val showNewUpdateDialog: (String, String, Boolean?) ->
                 // Create confirmation window
                 withUIContext {
                     AppUpdateNotifier.releasePageUrl = result.release.releaseLink
-                    showNewUpdateDialog(body, url, isBeta)
+                    NewUpdateDialogController(body, url, isBeta).showDialog(router)
                 }
             }
             is AppUpdateResult.NoNewUpdate -> {