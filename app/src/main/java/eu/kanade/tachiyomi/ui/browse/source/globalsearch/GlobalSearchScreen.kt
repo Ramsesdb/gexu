Ramsesdb/gexu

commit 66a2b8effe1fbc51ba0c44a97ad33857d297e93a

Fast browsing (#612)
* BrowseTab: serialized data object must have readResolve

* simplify usage of bulk-selection select-all/reverse

* hide bulk-selection select-all/reverse

* fast browsing by avoid insert every entries into DB

app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/globalsearch/GlobalSearchScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalHapticFeedback
 import cafe.adriel.voyager.core.model.rememberScreenModel
@@ -23,6 +24,7 @@ import eu.kanade.tachiyomi.ui.browse.ChangeMangasCategoryDialog
 import eu.kanade.tachiyomi.ui.browse.RemoveMangaDialog
 import eu.kanade.tachiyomi.ui.browse.source.browse.BrowseSourceScreen
 import eu.kanade.tachiyomi.ui.manga.MangaScreen
+import tachiyomi.core.common.util.lang.launchIO
 import tachiyomi.presentation.core.screens.LoadingScreen
 
 class GlobalSearchScreen(
@@ -51,6 +53,7 @@ class GlobalSearchScreen(
         }
 
         // KMK -->
+        val scope = rememberCoroutineScope()
         val bulkFavoriteScreenModel = rememberScreenModel { BulkFavoriteScreenModel() }
         val bulkFavoriteState by bulkFavoriteScreenModel.state.collectAsState()
 
@@ -93,20 +96,26 @@ class GlobalSearchScreen(
                 },
                 onClickItem = {
                     // KMK -->
-                    if (bulkFavoriteState.selectionMode) {
-                        bulkFavoriteScreenModel.toggleSelection(it)
-                    } else {
-                        // KMK <--
-                        navigator.push(MangaScreen(it.id, true))
+                    scope.launchIO {
+                        val manga = screenModel.networkToLocalManga.getLocal(it)
+                        if (bulkFavoriteState.selectionMode) {
+                            bulkFavoriteScreenModel.toggleSelection(manga)
+                        } else {
+                            // KMK <--
+                            navigator.push(MangaScreen(manga.id, true))
+                        }
                     }
                 },
-                onLongClickItem = { manga ->
+                onLongClickItem = {
                     // KMK -->
-                    if (!bulkFavoriteState.selectionMode) {
-                        bulkFavoriteScreenModel.addRemoveManga(manga, haptic)
-                    } else {
-                        // KMK <--
-                        navigator.push(MangaScreen(manga.id, true))
+                    scope.launchIO {
+                        val manga = screenModel.networkToLocalManga.getLocal(it)
+                        if (!bulkFavoriteState.selectionMode) {
+                            bulkFavoriteScreenModel.addRemoveManga(manga, haptic)
+                        } else {
+                            // KMK <--
+                            navigator.push(MangaScreen(manga.id, true))
+                        }
                     }
                 },
                 // KMK -->

app/src/main/java/eu/kanade/presentation/browse/BrowseSourceScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.paging.LoadState
@@ -24,7 +23,6 @@ import eu.kanade.presentation.browse.components.BrowseSourceList
 import eu.kanade.presentation.components.AppBar
 import eu.kanade.presentation.util.formattedMessage
 import eu.kanade.tachiyomi.source.Source
-import eu.kanade.tachiyomi.ui.browse.source.browse.BrowseSourceScreenModel
 import exh.metadata.metadata.RaisedSearchMetadata
 import exh.source.isEhBasedSource
 import kotlinx.collections.immutable.persistentListOf
@@ -62,7 +60,6 @@ fun BrowseSourceContent(
     onMangaLongClick: (Manga) -> Unit,
     // KMK -->
     selection: List<Manga>,
-    browseSourceState: BrowseSourceScreenModel.State,
     // KMK <--
 ) {
     val context = LocalContext.current
@@ -138,21 +135,8 @@ fun BrowseSourceContent(
         LoadingScreen(
             modifier = Modifier.padding(contentPadding),
         )
-        // KMK -->
-        browseSourceState.mangaDisplayingList.clear()
-        // KMK <--
         return
     }
-
-    // KMK -->
-    for (idx in browseSourceState.mangaDisplayingList.size..<mangaList.itemCount) {
-        mangaList[idx]?.collectAsState()?.value?.first?.let { manga ->
-            if (!browseSourceState.mangaDisplayingList.map { it.id }.contains(manga.id)) {
-                browseSourceState.mangaDisplayingList.add(manga)
-            }
-        }
-    }
-    // KMK <--
 
     // SY -->
     if (source?.isEhBasedSource() == true && ehentaiBrowseDisplayMode) {

app/src/main/java/eu/kanade/presentation/browse/components/RelatedMangasComfortableGrid.kt
@@ -80,7 +80,7 @@ fun RelatedMangasComfortableGrid(
                     )
                 }
                 items(
-                    key = { "related-comfort-${relatedManga.mangaList[it].id}" },
+                    key = { "related-comfort-${relatedManga.mangaList[it].url.hashCode()}" },
                     count = relatedManga.mangaList.size,
                 ) { index ->
                     val manga by getManga(relatedManga.mangaList[index])

app/src/main/java/eu/kanade/presentation/browse/components/RelatedMangasCompactGrid.kt
@@ -79,7 +79,7 @@ fun RelatedMangasCompactGrid(
                     )
                 }
                 items(
-                    key = { "related-compact-${relatedManga.mangaList[it].id}" },
+                    key = { "related-compact-${relatedManga.mangaList[it].url.hashCode()}" },
                     count = relatedManga.mangaList.size,
                 ) { index ->
                     val manga by getManga(relatedManga.mangaList[index])

app/src/main/java/eu/kanade/presentation/browse/components/RelatedMangasList.kt
@@ -82,7 +82,7 @@ fun RelatedMangasList(
                     )
                 }
                 items(
-                    key = { "related-list-${relatedManga.mangaList[it].id}" },
+                    key = { "related-list-${relatedManga.mangaList[it].url.hashCode()}" },
                     count = relatedManga.mangaList.size,
                 ) { index ->
                     val manga by getManga(relatedManga.mangaList[index])

app/src/main/java/eu/kanade/presentation/components/BulkSelectionToolbar.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.SelectAll
-import androidx.compose.material.icons.outlined.FlipToBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,6 +31,7 @@ fun BulkSelectionToolbar(
                 actions = persistentListOf<AppBar.AppBarAction>()
                     .builder()
                     .apply {
+                        /*
                         if (onSelectAll != null) {
                             add(
                                 AppBar.Action(
@@ -51,6 +50,7 @@ fun BulkSelectionToolbar(
                                 ),
                             )
                         }
+                         */
                         if (isRunning) {
                             add(
                                 AppBar.ActionCompose(

app/src/main/java/eu/kanade/presentation/manga/components/RelatedMangasRow.kt
@@ -66,7 +66,7 @@ fun RelatedMangaCardRow(
         contentPadding = PaddingValues(MaterialTheme.padding.small),
         horizontalArrangement = Arrangement.spacedBy(MaterialTheme.padding.extraSmall),
     ) {
-        items(mangas, key = { "related-row-${it.id}" }) {
+        items(mangas, key = { "related-row-${it.url.hashCode()}" }) {
             val manga by getManga(it)
             MangaItem(
                 title = manga.title,

app/src/main/java/eu/kanade/tachiyomi/ui/browse/BrowseTab.kt
@@ -39,6 +39,7 @@ import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
 data object BrowseTab : Tab {
+    private fun readResolve(): Any = BrowseTab
 
     override val options: TabOptions
         @Composable

commit 5b160f090aaa7402b0c5aa8ff19c9e0c4ba31270

Fix crash when open manga/chapter link caused by #612

app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/globalsearch/GlobalSearchScreen.kt
@@ -73,7 +73,12 @@ class GlobalSearchScreen(
                     is SearchItemResult.Success -> {
                         val manga = result.result.singleOrNull()
                         if (manga != null) {
-                            navigator.replace(MangaScreen(manga.id, true))
+                            // KMK -->
+                            scope.launchIO {
+                                val localManga = screenModel.networkToLocalManga.getLocal(manga)
+                                // KMK <--
+                                navigator.replace(MangaScreen(localManga.id, true))
+                            }
                         } else {
                             // Backoff to result screen
                             showSingleLoadingScreen = false